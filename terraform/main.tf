e7b68f7be71a1c20896e61ef3c5fc28353c6e108
abner espinoza <abespinoza155@gmail.com>
2025-07-30 16:48:14 -0400

    updating network

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,6 +59,6 @@ resource "docker_container" "ansible_vm1" {
 }
 
 # Create Docker network
-resource "docker_network" "semaphore_network" {
-  name = "semaphore_network"
+resource "docker_network" "monitoring-network" {
+  name = "monitoring-network"
 }
