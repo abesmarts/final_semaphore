c31c1cb9a31e832b7665d2a21b56a6fa27fff5f3
abner espinoza <abespinoza155@gmail.com>
2025-08-01 13:45:27 -0400

    fixing a lot of stuff

--- vm_deploy.sh
+++ vm_deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-tofu init
+
 tofu apply -auto-approve
 
 tofu output -json > tf_outputs.json
 
 VM_IP=$(jq -r '.vm1_ip.value' tf_outputs.json)
-VM_PORT=$(jq -r '.vm1_ssh_port.value'
-tf_outputs.json)
+VM_PORT=$(jq -r '.vm1_ssh_port.value' tf_outputs.json)
